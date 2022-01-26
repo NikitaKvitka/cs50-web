798948cba41acff99e686014bd43f91e8d5bd2e2
Nikita Kvitka <nikitakvitka@gmail.com>
2022-01-26 18:27:51 +0200

    1

---
+++ 5/learning.js
@@ -0,0 +1,67 @@
+let counter = 0;
+function count() {
+    counter++;
+    document.querySelector('h1').innerHTML = counter;
+   
+    if (counter % 10 === 0) {
+        alert(`Counter is now: ${counter}`);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('button').onclick = count;
+});
+
+
+// <!-- <!DOCTYPE html>
+// <html lang="en">
+//     <head>
+//         <title>Hello</title>
+//         <script>
+//             function hello () {
+//                 alert('Hello, world.');
+//             }
+            
+//         </script>
+//     </head>
+//     <body>
+//         <h1>Hello</h1>
+//         <button onclick="hello()">Click</button>
+//     </body>
+// </html> -->
+// <!-- ---------------------------------------------- -->
+
+// <!DOCTYPE html>
+// <html lang="en">
+//     <head>
+//         <title>Counter</title>
+//         <script src="learning.js"></script>
+//     </head>
+//     <body>
+//         <h1>0</h1>
+//         <button>Count</button>
+//     </body>
+// </html>
+// <!-- ---------------------------------------------- -->
+
+// <!-- <!DOCTYPE html>
+// <html lang="en">
+//     <head>
+//         <title>Hello</title>
+//         <script>
+//             function hello () {
+//                 const heading = document.querySelector('h1');
+//                 if (heading.innerHTML === 'Hello') {
+//                     heading.innerHTML = 'Goodbye';
+//                 } else {
+//                     heading.innerHTML = 'Hello';
+//                 }
+//             }
+            
+//         </script>
+//     </head>
+//     <body>
+//         <h1>Hello</h1>
+//         <button onclick="hello()">Click</button>
+//     </body>
+// </html> -->
