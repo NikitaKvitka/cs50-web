6b53aae9a513f721e3e62c223c5e9f888fa556ec
Nikita Kvitka <nikitakvitka@gmail.com>
2022-01-28 15:58:12 +0200

    leaening

--- 5/learning.js
+++ 5/learning.js
@@ -1,16 +1,16 @@
-let counter = 0;
-function count() {
-    counter++;
-    document.querySelector('h1').innerHTML = counter;
+// let counter = 0;
+// function count() {
+//     counter++;
+//     document.querySelector('h1').innerHTML = counter;
    
-    if (counter % 10 === 0) {
-        alert(`Counter is now: ${counter}`);
-    }
-}
+//     if (counter % 10 === 0) {
+//         alert(`Counter is now: ${counter}`);
+//     }
+// }
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('button').onclick = count;
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.querySelector('button').onclick = count;
+// });
 
 
 // <!-- <!DOCTYPE html>
